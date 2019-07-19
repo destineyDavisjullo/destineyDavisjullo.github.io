46064aabb99437167a32de51ee069e483227c153
destineyDavisjullo <53087354+destineyDavisjullo@users.noreply.github.com>
2019-07-19 06:49:17 -0700

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,11 @@
+alert("Welcome to my page chickens!")
+
+function bigImg(x) {
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+
+function smallimg(x) {
+  x.style.height = "100px";
+  x.style.width = "100PX";
+}
